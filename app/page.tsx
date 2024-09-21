a25127890c7028170bbf843104c934bf2dcb26b9
Vansh-Parate <vanshparate@gmail.com>
2024-09-21 11:56:18 +0530

    start

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Navigationbar from "@/components/main/Navbar"
+
+
+
+export default function Home() {
+ 
+  return (
+    <div className=" flex flex-col w-full font-inter ">
+      <Navigationbar />
+      
+    </div>
+  );
+}
---
+++ components/main/Navbar.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { FiGithub } from "react-icons/fi";
+import {motion} from "framer-motion";
+
+const Navigationcontent=[{
+  id:"1", 
+  name:"Home"
+},{
+  id:"2",
+  name:"About"
+},
+{
+  id:"3",
+  name:"Projects"
+},
+{
+  id:"4",
+  name:"Skills"
+},
+{
+  id:"5",
+  name:"Contact"
+}]
+
+export default function Navigationbar() {
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  return (
+    <motion.div initial={{opacity:0.2}} animate={{opacity:1}} transition={{delay:0.4,duration:2}} className="flex justify-center max-w-screen">
+      <div className="w-11/12 2xl:max-w-7xl   py-4 z-50 fixed top-2 h-12 md:h-16 2xl:h-20 flex items-center md:justify-between rounded-full bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90  shadow-lg shadow-black drop-shadow-lg">
+        <div className=" flex md:flex-none flex-grow justify-around text-xs md:text-sm text-white  md:gap-8 lg:gap-12 mx-2 md:ml-10">
+          {Navigationcontent.map((nav)=>{
+
+         return  <div key={nav.id}
+            className="cursor-pointer transform transition hover:scale-110 text-md 2xl:text-xl hover:text-gray-400"
+            onClick={() => scrollToSection(nav.id)}
+          >
+            {nav.name}
+          </div>
+          })}
+        </div>
+        <div className="md:flex md:justify-between gap-6 hidden   md:gap-10 items-center mr-6 md:mr-12  lg:text-xl">
+          <a href={"https://github.com/Vansh-Parate"} target="_blank">
+            <div>
+              <FiGithub  className="cursor-pointer transform transition hover:scale-110 text-white hover:text-gray-400"/>
+            </div>
+          </a>
+          </div>
+      </div>
+    </motion.div>
+  );
+}
